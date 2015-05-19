c7318d38ce85f058c47ff111c4061a7b49034cb1
thejsj <jorge.silva@thejsj.com>
2015-05-19 12:53:20 -0700

    Add step-2

--- server/main.rb
+++ server/main.rb
@@ -2,72 +2,47 @@ require 'rubygems'
 require 'bundler/setup'
 
 require 'rethinkdb'
-require 'eventmachine'
-require 'faye'
-require 'faye/websocket'
 require 'sinatra'
 
 include RethinkDB::Shortcuts
-Faye::WebSocket.load_adapter "thin"
 
-class Faye::Server
-  def scheme; end
+Conn = r.connect host: "localhost", port: 28015
+Conn.use "rethink_chat"
+
+# Create Database
+begin
+  r.db_create("rethink_chat").run(Conn)
+rescue RethinkDB::RqlRuntimeError; end
+
+# Add Tables and Indexes
+begin
+  r.db("rethink_chat").table_create("messages").run(Conn)
+rescue RethinkDB::RqlRuntimeError; end
+
+begin
+  r.db("rethink_chat").table("messages").index_create("created").run(Conn)
+rescue RethinkDB::RqlRuntimeError; end
+
+# HTTP
+class MessageApp < Sinatra::Base
+  # We presume you're running the app from the repo's top directory
+  set :public_folder, 'client'
+  configure do
+    set :threaded, true
+  end
+
+  get "/" do
+    redirect "/index.html"
+  end
+
+  get '/messages' do
+    r
+      .table("messages")
+      .order_by({ index: "created" })
+      .coerce_to("array")
+      .run(Conn)
+      .to_json
+  end
 end
 
-EM.run do
-
-  Conn = r.connect host: "localhost", port: 28015
-  Conn.use "rethink_chat"
-
-  # Create Database
-  begin
-    r.db_create("rethink_chat").run(Conn)
-  rescue RethinkDB::RqlRuntimeError; end
-
-  # Add Tables and Indexes
-  begin
-    r.db("rethink_chat").table_create("messages").run(Conn)
-  rescue RethinkDB::RqlRuntimeError; end
-
-  begin
-    r.db("rethink_chat").table("messages").index_create("created").run(Conn)
-  rescue RethinkDB::RqlRuntimeError; end
-
-  # HTTP
-  class MessageApp < Sinatra::Base
-    # We presume you're running the app from the repo's top directory
-    set :public_folder, 'client'
-    configure do
-      set :threaded, true
-    end
-
-    get "/" do
-      redirect "/index.html"
-    end
-
-    get '/messages' do
-      r
-        .table("messages")
-        .order_by({ index: "created" })
-        .coerce_to("array")
-        .run(Conn)
-        .to_json
-    end
-  end
-
-  # Add Faye
-  App = Faye::RackAdapter.new MessageApp, mount: "/faye"
-
-  # Changefeed listener
-  r.table("messages").changes.em_run(Conn) do |err, change|
-    App.get_client.publish('/messages/new', change["new_val"])
-  end
-
-  # Send all messages at the beginning
-  App.get_client.subscribe "/messages/add" do |m|
-    r.table("messages").insert(m).run(Conn)
-  end
-
-  Rack::Server.start app: App, Port: 8000
-end
-
+Rack::Server.start app: MessageApp, Port: 8000
